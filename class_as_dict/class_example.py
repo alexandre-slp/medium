fcf5a0c7f83f8ac8f546cc8f972e8bf8a26f3c2b
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2020-02-14 15:58:04 -0300

    Added full code

--- class_as_dict/class_example.py
+++ class_as_dict/class_example.py
@@ -1,3 +1,13 @@
-class Class:
+class Class(dict):
     foo = 'foo'
     bar = 'bar'
+
+    def __repr__(self):
+        self.__setitem__('foo', self.foo)
+        self.__setitem__('bar', self.bar)
+        return super().__repr__()
+
+c = Class()
+c
+# Output:
+{'foo': 'foo', 'bar': 'bar'}
